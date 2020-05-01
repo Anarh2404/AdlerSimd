5ce0cb593e11ed89adb5c5174a767d86398be25c
evgeny.p <anarh2404@outlook.com>
2020-05-01 14:45:55 +0300

    cleanup adler32 tests

--- AdlerHash/AdlerHashTest/Adler32Test.cs
+++ AdlerHash/AdlerHashTest/Adler32Test.cs
@@ -7,147 +7,168 @@ namespace AdlerHashTest
     public class Adler32Test
     {
         [Theory]
-        [InlineData(1024)]
-        [InlineData(1024 * 1024)]
-        [InlineData(32 * 1024 * 1024)]
-        [InlineData(128 * 1024 * 1024)]
-        [InlineData(363898431)]
-        [InlineData(363898432)]
-        [InlineData(1024 * 1024 * 1024)]
-        public void AdlerAlignmentSseTest(int size)
+        [InlineData(1024, 3838443024)]
+        [InlineData(1024 * 1024, 1185183625)]
+        [InlineData(32 * 1024 * 1024, 2524836307)] 
+        [InlineData(128 * 1024 * 1024, 4178691958)]
+        [InlineData(363898432, 3303655557)] 
+        [InlineData(1024 * 1024 * 1024, 3590863875)]
+        public void AdlerAlignedOptimizedTest(int size, uint expected)
         {
             var bytes = Enumerable.Range(0, size).Select(i => (byte)i).ToArray();
+            
+            
+            var hash = AdlerHash.Adler32.GetSimpleOptimized(bytes, 1, 0);
 
-            var simpleHash = AdlerHash.Adler32.GetSimple(bytes, 1, 0);
-            var sseHash = AdlerHash.Adler32.GetSse(bytes, 1, 0);
-
-            Assert.Equal(simpleHash, sseHash);
+            
+            Assert.Equal(expected, hash);
         }
-
+        
         [Theory]
-        [InlineData(1024)]
-        [InlineData(1024 * 1024)]
-        [InlineData(32 * 1024 * 1024)]
-        [InlineData(128 * 1024 * 1024)]
-        [InlineData(363898431)]
-        [InlineData(363898432)]
-        [InlineData(1024 * 1024 * 1024)]
-        public void AdlerAlignmentSseTest2(int size)
+        [InlineData(1024, 3838443024)]
+        [InlineData(1024 * 1024, 1185183625)]
+        [InlineData(32 * 1024 * 1024, 2524836307)] 
+        [InlineData(128 * 1024 * 1024, 4178691958)]
+        [InlineData(363898432, 3303655557)] 
+        [InlineData(1024 * 1024 * 1024, 3590863875)]
+        public void AdlerAlignedOptimizedTwoPassesTest(int size, uint expected)
         {
             ReadOnlySpan<byte> bytes = Enumerable.Range(0, size).Select(i => (byte)i).ToArray();
-
             uint s1 = 1;
             uint s2 = 0;
-            var simpleHash = AdlerHash.Adler32.GetSimple(bytes.Slice(0, size / 2), s1, s2);
-            s1 = simpleHash & 0xffff;
-            s2 = simpleHash >> 16;
-            var simpleHash2 = AdlerHash.Adler32.GetSimple(bytes.Slice(size / 2), s1, s2);
-            var simpleHash3 = AdlerHash.Adler32.GetSimple(bytes, 1, 0);
+
+            
+            var hash = AdlerHash.Adler32.GetSimpleOptimized(bytes.Slice(0, size / 2), s1, s2);
+            s1 = hash & 0xffff;
+            s2 = hash >> 16;
+            hash = AdlerHash.Adler32.GetSimpleOptimized(bytes.Slice(size / 2), s1, s2);
 
 
-
-            s1 = 1;
-            s2 = 0;
-            var sseHash = AdlerHash.Adler32.GetSse(bytes.Slice(0, size / 2), s1, s2);
-            s1 = sseHash & 0xffff;
-            s2 = sseHash >> 16;
-            var sseHash2 = AdlerHash.Adler32.GetSse(bytes.Slice(size / 2), s1, s2);
-            var sseHash3 = AdlerHash.Adler32.GetSse(bytes, 1, 0);
-
-            Assert.Equal(simpleHash2, simpleHash3);
-            Assert.Equal(sseHash2, sseHash3);
-            Assert.Equal(simpleHash3, sseHash3);
+            Assert.Equal(expected, hash);
         }
-
+        
         [Theory]
-        [InlineData(1024)]
-        [InlineData(1024 * 1024)]
-        [InlineData(32 * 1024 * 1024)]
-        [InlineData(128 * 1024 * 1024)]
-        [InlineData(363898431)]
-        [InlineData(363898432)]
-        [InlineData(1024 * 1024 * 1024)]
-        public void AdlerAlignmentOptimizedTest2(int size)
+        [InlineData(1024 +3, 3744136723)]
+        [InlineData(1024 * 1024 + 7, 2345170846)]
+        [InlineData(32 * 1024 * 1024 + 11, 4227920394)] 
+        [InlineData(128 * 1024 * 1024 + 13, 471910340)]
+        [InlineData(363898431, 4233480262)]
+        [InlineData(1024 * 1024 * 1024 + 27, 932068706)]
+        public void AdlerNotAlignedOptimizedTest(int size, uint expected)
+        {
+            var bytes = Enumerable.Range(0, size).Select(i => (byte)i).ToArray();
+            
+            
+            var hash = AdlerHash.Adler32.GetSimpleOptimized(bytes, 1, 0);
+
+            
+            Assert.Equal(expected, hash);
+        }
+        
+        [Theory]
+        [InlineData(1024 +3, 3744136723)]
+        [InlineData(1024 * 1024 + 7, 2345170846)]
+        [InlineData(32 * 1024 * 1024 + 11, 4227920394)] 
+        [InlineData(128 * 1024 * 1024 + 13, 471910340)]
+        [InlineData(363898431, 4233480262)]
+        [InlineData(1024 * 1024 * 1024 + 27, 932068706)]
+        public void AdlerNotAlignedOptimizedTwoPassesTest(int size, uint expected)
         {
             ReadOnlySpan<byte> bytes = Enumerable.Range(0, size).Select(i => (byte)i).ToArray();
-
             uint s1 = 1;
             uint s2 = 0;
-            var simpleHash = AdlerHash.Adler32.GetSimple(bytes.Slice(0, size / 2), s1, s2);
-            s1 = simpleHash & 0xffff;
-            s2 = simpleHash >> 16;
-            var simpleHash2 = AdlerHash.Adler32.GetSimple(bytes.Slice(size / 2), s1, s2);
-            var simpleHash3 = AdlerHash.Adler32.GetSimple(bytes, 1, 0);
+
+            
+            var hash = AdlerHash.Adler32.GetSimpleOptimized(bytes.Slice(0, size / 2), s1, s2);
+            s1 = hash & 0xffff;
+            s2 = hash >> 16;
+            hash = AdlerHash.Adler32.GetSimpleOptimized(bytes.Slice(size / 2), s1, s2);
 
 
-
-            s1 = 1;
-            s2 = 0;
-            var sseHash = AdlerHash.Adler32.GetSimpleOptimized(bytes.Slice(0, size / 2), s1, s2);
-            s1 = sseHash & 0xffff;
-            s2 = sseHash >> 16;
-            var sseHash2 = AdlerHash.Adler32.GetSimpleOptimized(bytes.Slice(size / 2), s1, s2);
-            var sseHash3 = AdlerHash.Adler32.GetSimpleOptimized(bytes, 1, 0);
-
-            Assert.Equal(simpleHash2, simpleHash3);
-            Assert.Equal(sseHash2, sseHash3);
-            Assert.Equal(simpleHash3, sseHash3);
+            Assert.Equal(expected, hash);
         }
-
+        
         [Theory]
-        [InlineData(1024)]
-        [InlineData(1024 * 1024)]
-        [InlineData(32 * 1024 * 1024)]
-        [InlineData(128 * 1024 * 1024)]
-        [InlineData(363898431)]
-        [InlineData(363898432)]
-        [InlineData(1024 * 1024 * 1024)]
-        public void AdlerNotAlignmentSseTest(int size)
+        [InlineData(1024, 3838443024)]
+        [InlineData(1024 * 1024, 1185183625)]
+        [InlineData(32 * 1024 * 1024, 2524836307)] 
+        [InlineData(128 * 1024 * 1024, 4178691958)]
+        [InlineData(363898432, 3303655557)] 
+        [InlineData(1024 * 1024 * 1024, 3590863875)]
+        public void AdlerAlignedSseTest(int size, uint expected)
         {
             var bytes = Enumerable.Range(0, size).Select(i => (byte)i).ToArray();
+            
+            
+            var hash = AdlerHash.Adler32.GetSse(bytes, 1, 0);
 
-            var simpleHash = AdlerHash.Adler32.GetSimple(bytes, 1, 0);
-            var sseHash = AdlerHash.Adler32.GetSse(bytes, 1, 0);
-
-            Assert.Equal(simpleHash, sseHash);
+            
+            Assert.Equal(expected, hash);
         }
 
-
+        
         [Theory]
-        [InlineData(1024)]
-        [InlineData(1024 * 1024)]
-        [InlineData(32 * 1024 * 1024)]
-        [InlineData(128 * 1024 * 1024)]
-        [InlineData(363898431)]
-        [InlineData(363898432)]
-        [InlineData(1024 * 1024 * 1024)]
-        public void AdlerAlignmentSimple2Test(int size)
+        [InlineData(1024, 3838443024)]
+        [InlineData(1024 * 1024, 1185183625)]
+        [InlineData(32 * 1024 * 1024, 2524836307)] 
+        [InlineData(128 * 1024 * 1024, 4178691958)]
+        [InlineData(363898432, 3303655557)] 
+        [InlineData(1024 * 1024 * 1024, 3590863875)]
+        public void AdlerAlignedSseTwoPassesTest(int size, uint expected)
         {
-            var bytes = Enumerable.Range(0, size).Select(i => (byte)i).ToArray();
+            ReadOnlySpan<byte> bytes = Enumerable.Range(0, size).Select(i => (byte)i).ToArray();
+            uint s1 = 1;
+            uint s2 = 0;
+            
+            
+            var hash = AdlerHash.Adler32.GetSse(bytes.Slice(0, size / 2), s1, s2);
+            s1 = hash & 0xffff;
+            s2 = hash >> 16;
+            hash = AdlerHash.Adler32.GetSse(bytes.Slice(size / 2), s1, s2);
 
-            var simpleHash = AdlerHash.Adler32.GetSimple(bytes, 1, 0);
-            var sseHash = AdlerHash.Adler32.GetSimpleOptimized(bytes, 1, 0);
-
-            Assert.Equal(simpleHash, sseHash);
+            
+            Assert.Equal(expected, hash);
         }
-
-
+        
         [Theory]
-        [InlineData(1024)]
-        [InlineData(1024 * 1024)]
-        [InlineData(32 * 1024 * 1024)]
-        [InlineData(128 * 1024 * 1024)]
-        [InlineData(363898431)]
-        [InlineData(363898432)]
-        [InlineData(1024 * 1024 * 1024)]
-        public void AdlerNotAlignmentSimple2Test(int size)
+        [InlineData(1024 +3, 3744136723)]
+        [InlineData(1024 * 1024 + 7, 2345170846)]
+        [InlineData(32 * 1024 * 1024 + 11, 4227920394)] 
+        [InlineData(128 * 1024 * 1024 + 13, 471910340)]
+        [InlineData(363898431, 4233480262)]
+        [InlineData(1024 * 1024 * 1024 + 27, 932068706)]
+        public void AdlerNotAlignedSseTest(int size, uint expected)
         {
             var bytes = Enumerable.Range(0, size).Select(i => (byte)i).ToArray();
+            
+            
+            var hash = AdlerHash.Adler32.GetSse(bytes, 1, 0);
 
-            var simpleHash = AdlerHash.Adler32.GetSimple(bytes, 1, 0);
-            var sseHash = AdlerHash.Adler32.GetSimpleOptimized(bytes, 1, 0);
+            
+            Assert.Equal(expected, hash);
+        }
+        
+        [Theory]
+        [InlineData(1024 +3, 3744136723)]
+        [InlineData(1024 * 1024 + 7, 2345170846)]
+        [InlineData(32 * 1024 * 1024 + 11, 4227920394)] 
+        [InlineData(128 * 1024 * 1024 + 13, 471910340)]
+        [InlineData(363898431, 4233480262)]
+        [InlineData(1024 * 1024 * 1024 + 27, 932068706)]
+        public void AdlerNotAlignedSseTwoPassesTest(int size, uint expected)
+        {
+            ReadOnlySpan<byte> bytes = Enumerable.Range(0, size).Select(i => (byte)i).ToArray();
+            uint s1 = 1;
+            uint s2 = 0;
 
-            Assert.Equal(simpleHash, sseHash);
+            
+            var hash = AdlerHash.Adler32.GetSse(bytes.Slice(0, size / 2), s1, s2);
+            s1 = hash & 0xffff;
+            s2 = hash >> 16;
+            hash = AdlerHash.Adler32.GetSse(bytes.Slice(size / 2), s1, s2);
+
+
+            Assert.Equal(expected, hash);
         }
     }
 }
